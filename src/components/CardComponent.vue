e29d1ae7b005efb1bea5f8eaf0635b2f7a2a9305
nina <kana202206@gmail.com>
2023-02-28 15:14:04 +0800

    git add

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <!-- <div> -->
+        這是一個卡片元件 {{ text }}
+    <!-- </div> -->
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            text: 'ABC',
+        }
+    }
+}
+</script>
