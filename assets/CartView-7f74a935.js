7df64cc40720d8b340f28e9fb72f2403508aad19
nina <kana202206@gmail.com>
2023-03-03 21:45:15 +0800

    fix bug

---
+++ assets/CartView-7f74a935.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-2cfd040b.js";const e={};function t(c,_){return" 這是前台購物車列表頁面 "}const o=r(e,[["render",t]]);export{o as default};
---
+++ assets/HomeView-72a9197e.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,d as t}from"./index-2cfd040b.js";const c={components:{}},n={class:"home"},a=t("h1",null,"Hello, This is Home Page.",-1),_=[a];function r(i,d,l,p,m,h){return o(),s("div",n,_)}const u=e(c,[["render",r]]);export{u as default};
